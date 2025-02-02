78ba3d0cbc5acbf861d208169aa3b9c71a835284
mahmoodnezhad <mahmoodnezhadjafar@gmail.com>
2025-02-02 23:41:27 +0330

    part8 component in react

--- src/index.js
+++ src/index.js
@@ -2,19 +2,37 @@ import React from 'react';
 // import ReactDOM from "react-dom"
 import { createRoot } from 'react-dom/client';
 // let elem = <h1>پروژه ی تایمر</h1>
-
 // ReactDOM.render(elem, document.getElementById("root"));
  
-const tick = ()=>{
-    const elem = (
-        <div>
-        <h1> پروژه ی تایمر </h1>
-        <h2>
-                it is {new Date().toLocaleTimeString()}
+class Timer extends React.Component {
+     render(){
+        return(
+            <h2>
+            it is  {new Date().toLocaleTimeString()}
         </h2>
-        </div>
-    )
-    createRoot(document.getElementById("root")).render(elem);
+        )
+     }
+}
+class Hello extends React.Component {
+    render(){
+        return(
+            <h1>سلام دوستان</h1>
+        )
+    }
+}
+class App extends React.Component {
+     render(){
+        return (
+            <div>
+            <Hello/>
+            <Timer/>
+            </div>
+        )
+     }
+}
+
+const tick = ()=>{
+    createRoot(document.getElementById("root")).render(<App/>);
 }
 setInterval(()=>{
     tick();
