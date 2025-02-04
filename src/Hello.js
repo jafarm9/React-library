301cec2729834714a59c0348ca4bb78233cfea86
mahmoodnezhad <mahmoodnezhadjafar@gmail.com>
2025-02-04 23:31:02 +0330

    part10 create componetnsiv react

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+class Hello extends React.Component {
+    render(){
+        return(
+            <h1>سلام دوستان</h1>
+        )
+    }
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,53 +1,12 @@
 import React from 'react';
 // import ReactDOM from "react-dom"
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';          // از ری اکت 2018 به بعد به این شکل فراخوانی میشه
 import './style.css'
 // let elem = <h1>پروژه ی تایمر</h1>
-// ReactDOM.render(elem, document.getElementById("root"));
- 
-class Timer extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            time : new Date().toLocaleTimeString()
-        }
-    }
-     render(){
-        setInterval(()=>{
-            this.setState({
-                time: new Date().toLocaleTimeString()
-            })
-        }, 1000)
-        return(
-            <h2 className='timer'>
-            it is  {this.state.time}
-        </h2>
-        )
-     }
-}
-class Hello extends React.Component {
-    render(){
-        return(
-            <h1>سلام دوستان</h1>
-        )
-    }
-}
-class App extends React.Component {
-     render(){
-        return (
-            <div className='main'>
-            <Hello/>
-            <Timer/>
-            </div>
-        )
-     }
-}
+// ReactDOM.render(elem, document.getElementById("root"));       // فراخوانی ری اکت قبل از 2018
+ import App from "./App"
 
-// const tick = ()=>{
-    createRoot(document.getElementById("root")).render(<App/>);
-// }
-// setInterval(()=>{
-//     tick();
-// },1000)
+
+ createRoot(document.getElementById("root")).render(<App/>);
 
 
