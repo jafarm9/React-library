51c70a8129eedb32718aee469d63775c928da998
mahmoodnezhad <mahmoodnezhadjafar@gmail.com>
2025-02-05 16:43:02 +0330

    part11 component DIDMount and ...in react

--- src/Timer.js
+++ src/Timer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css'
 
+
+var interval;
 class Timer extends React.Component {
     constructor(){
         super();
@@ -9,12 +11,22 @@ class Timer extends React.Component {
             time : new Date().toLocaleTimeString()
         }
     }
-     render(){
-        setInterval(()=>{
+    componentDidMount(){                                           // انواع چرخه ی حیاط در ری اکت
+        interval = setInterval(()=>{
+            console.log("componentDidMount");
+            
             this.setState({
-                time: new Date().toLocaleTimeString()
+                time: new Date().toLocaleTimeString()          // انواع چرخه ی حیاط در ری اکت 
             })
         }, 1000)
+    } 
+    componentDidUpdate(){                                             // انواع چرخه ی حیاط در ری اکت
+        console.log(this.state.time);
+        
+    }
+     render(){
+        console.log("componentDidMount");
+        
         return(
             <h2 className='timer'>
             it is  {this.state.time}
